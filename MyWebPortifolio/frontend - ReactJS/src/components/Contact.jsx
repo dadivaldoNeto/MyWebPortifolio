5f5f1573ba27c3bf3c925879f3e5f5aa19593bdb
Bruno de Fraga <brunofragaa97@gmail.com>
2025-09-16 12:15:31 -0300

    ajustado styles dos componentes

--- MyWebPortifolio/frontend - ReactJS/src/components/Contact.jsx
+++ MyWebPortifolio/frontend - ReactJS/src/components/Contact.jsx
@@ -3,11 +3,24 @@ import "../styles/contact.css";
 
 const Contact = () => {
   return (
-    <section className="contact">
+    <section className="contact" id="contact">
       <h2>Contato</h2>
       <p>Entre em contato comigo:</p>
-      <p>Email: brunofraga@email.com</p>
-      <p>LinkedIn: linkedin.com/in/brunofraga</p>
+      
+      <div className="contact-links">
+        <a href="mailto:brunofraga@email.com">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+            <path d="M4 4h16v16H4V4zm8 9l8-5H4l8 5z"/>
+          </svg>
+          Email: brunofraga@email.com
+        </a>
+        <a href="https://linkedin.com/in/brunofraga" target="_blank" rel="noopener noreferrer">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+            <path d="M4 4h16v16H4V4zm4 14h2v-6H8v6zm1-7a1 1 0 100-2 1 1 0 000 2zm4 7h2v-3c0-1 0-3-2-3s-2 2-2 3v3z"/>
+          </svg>
+          LinkedIn: linkedin.com/in/brunofraga
+        </a>
+      </div>
     </section>
   );
 };
