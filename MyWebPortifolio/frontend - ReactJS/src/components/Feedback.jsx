15df5bad59a4d7339be6d97eb3f5f8d1b4b38afe
Bruno de Fraga <brunofragaa97@gmail.com>
2025-09-22 14:05:44 -0300

    ajustado import no componente feedback

--- MyWebPortifolio/frontend - ReactJS/src/components/Feedback.jsx
+++ MyWebPortifolio/frontend - ReactJS/src/components/Feedback.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import "../styles//feedback.css";
+import "../styles/feedback.css";
 
 const Feedback = () => {
     const [rating, setRating] = useState(0);
